9afbed159f4998449a3d0e4e318b71c4e7140b78
JonathanHelvey <jonathanhelvey27@gmail.com>
2019-04-24 22:02:39 -0500

    Google Maps WOrking

--- App.js
+++ App.js
@@ -1,12 +1,65 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+//import Map from "./components/Map";
+import { MapView } from "expo";
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: true,
+      markers: []
+    };
+  }
+  fetchMarkerData() {
+    fetch("https://feeds.citibikenyc.com/stations/stations.json")
+      .then(response => response.json())
+      .then(responseJson => {
+        this.setState({
+          isLoading: false,
+          markers: responseJson.stationBeanList
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+  componentDidMount() {
+    this.fetchMarkerData();
+  }
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <MapView
+        style={{
+          flex: 1
+        }}
+        initialRegion={{
+          latitude: 41.8781,
+          longitude: -87.6298,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421
+        }}>
+        {this.state.isLoading
+          ? null
+          : this.state.markers.map((marker, index) => {
+              const coords = {
+                latitude: marker.latitude,
+                longitude: marker.longitude
+              };
+
+              const metadata = `Status: ${marker.statusValue}`;
+
+              return (
+                <MapView.Marker
+                  key={index}
+                  coordinate={coords}
+                  title={marker.stationName}
+                  description={metadata}
+                />
+              );
+            })}
+      </MapView>
     );
   }
 }
@@ -14,8 +67,8 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
