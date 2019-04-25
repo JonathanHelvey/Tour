18950eeb0afc6f7fdd4200d1d6652b9830799815
JonathanHelvey <jonathanhelvey27@gmail.com>
2019-04-24 22:36:16 -0500

    Tour Delete Files

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, Image, SafeAreaView } from "react-native";
 //import Map from "./components/Map";
 import { MapView } from "expo";
 
